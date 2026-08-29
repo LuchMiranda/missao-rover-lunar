4979c42615d7eb03ace54fe405378fd8b53bc239
Lucas H. Miranda <lucashenriquemiranda1@gmail.com>
2026-08-28 21:13:10 -0300

    Add files via upload

---
+++ Rover Lunar.java
@@ -0,0 +1,9 @@
+public class rover {
+    public static void inicializarRover() {
+        System.out.println ("Sistemas Rover Iniciados!! ");
+        System.out.println ("Paineis Solares: OK");
+        System.out.println ("Níveis de bateria: 100%");
+    }
+    public static void main (String []args){
+    inicializarRover ();
+    } 
